2e8fe847f155572c79dc390e336bb399f8492dea
Əli Hacıyev <elihcyv@gmail.com>
2026-06-21 23:38:05 +0400

    Change model name from ConvNeXt_Small to ResNet18

--- dl_brain_cancer_project/config.py
+++ dl_brain_cancer_project/config.py
@@ -9,4 +9,4 @@ class CFG:
     NUM_CLASSES = 3
     ROOT_DIR = '/kaggle/input/datasets/orvile/brain-cancer-mri-dataset/Brain_Cancer raw MRI data/Brain_Cancer'
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    MODEL_NAME = 'ConvNeXt_Snall'
+    MODEL_NAME = 'ResNet18'
